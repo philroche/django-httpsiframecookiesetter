a38f44f678d0fa550b3f6092a70a03a2e4b33a15
Philip Roche <phil@tinyviking.ie>
2013-10-09 12:52:26 +0100

    use root_urlconf for reverse looking up the cookie setter url

--- httpsiframecookiesetter/middleware.py
+++ httpsiframecookiesetter/middleware.py
@@ -12,16 +12,31 @@ import urllib2
 
 from .settings import HTTPS_IFRAME_COOKIESETTER_URL_TO_CHECK
 
+
 logger = logging.getLogger(__name__)
 
 class CookieSetterMiddleware(object):
+    def __init__(self, *args, **kwargs):
+        super(CookieSetterMiddleware, self).__init__(*args, **kwargs)
+        self._property_cache = {}
+
+    def urlpath(self):
+        if not self._property_cache.has_key('cookiesetter_view_path'):
+            cookiesetter_view_path = reverse('cookiesetter', urlconf=settings.ROOT_URLCONF)
+            self._property_cache['cookiesetter_view_path'] = cookiesetter_view_path
+        return self._property_cache['cookiesetter_view_path']
+
+    def clear_property_cache(self):
+        self._property_cache = {}
 
     def process_request(self, request):
-        #
+
         #get the url to the cookiesetter view
-        cookiesetter_view_path = reverse('cookiesetter', urlconf='urls')
+        #cached value
+        cookiesetter_view_path = self.urlpath()
+
+
         #TODO we need to check the number of redirects in case we end up in a loop for some reason
-        
         if request.is_secure() and not (request.path.startswith(settings.MEDIA_URL) or request.path.startswith(settings.STATIC_URL)) and cookiesetter_view_path not in request.path and HTTPS_IFRAME_COOKIESETTER_URL_TO_CHECK in request.path:
             user_agent = request.META.get('HTTP_USER_AGENT', '')
 
